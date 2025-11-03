7034ff935204fef29a688b291d6a829f662d04cf
Omar Hamdan <omar.hamdan@post.runi.ac.il>
2025-11-03 10:59:03 +0200

    last

--- Bill3.java
+++ Bill3.java
@@ -3,11 +3,14 @@ public class Bill3 {
 	public static void main(String[] args) {
 		// To get you started, here is the first line in the program:
 	    // Replace this comment with the rest of your code
-		System.out.println("Splitting a restaurant bill evenly among three diners.");
 	    String name1 = args[0];
 	    String name2 = args[1];
 	    String name3 = args[2];
 		int bill = Integer.parseInt(args[3]);
-		System.out.println("Dear " + name1 + ", " + name2 + " and " + name3 + ": pay " + (double)(bill / 3) + " Shekels each.");
+		int share = bill / 3;
+		if (bill % 3 != 0){
+			share += 1;
+		}
+		System.out.println("Dear " + name1 + ", " + name2 + " and " + name3 + ": pay " + (double)(share) + " Shekels each.");
 	}
 }
